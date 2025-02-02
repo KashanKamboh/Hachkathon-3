a39b9fb3eb42aa6f8ed2ea795653082c7e27bb2c
Kashaf Tariq <160773370+KashanKamboh@users.noreply.github.com>
2025-02-02 23:58:49 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["cdn.sanity.io"], // Add the Sanity image domain here
+      },
+};
+
+export default nextConfig;
+
+
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+
+import Hero from "./components/Hero";
+import Brand from "./Brand";
+import Ceremics from "./components/Ceremics";
+import Benefit from "./components/Benefit";
+import Touch from "./components/Touch";
+import Popular from "./components/Popular";
+
+
+
+
+export default function Home(){
+  return(
+    <div>
+      
+      
+    
+      <Hero/>
+      
+      <Brand/>
+      <Ceremics/>
+      <Popular/>
+      <Benefit/>
+      <Touch/>
+      
+    </div>
+  )
+}
